b6745d597937b462271c5926d989e0b01fd8fe78
Joshua-Kloepfer20 <jkloepfer20@stuy.edu>
2022-01-16 13:39:34 -0500

    wor

--- client.c
+++ client.c
@@ -10,8 +10,22 @@
 #include <netdb.h>
 #include <SDL2/SDL.h>
 
+#define WHITEPAWN 1
+#define WHITEKING 2
+#define WHITEQUEEN 3
+#define WHITEBISHOP 4
+#define WHITEKNIGHT 5
+#define WHITEROOK 6
+#define BLACKPAWN 7
+#define BLACKKING 8
+#define BLACKQUEEN 9
+#define BLACKBISHOP 10
+#define BLACKKNIGHT 11
+#define BLACKROOK 12
+
 struct user {char username[50]; char password[50]; int rating; int logged; };
 struct message {char username[50]; char messsage[50]; };
+struct square {SDL_Rect rect; SDL_Surface * image; };
 
 int client_handshake() {
   struct addrinfo * hints;
@@ -27,23 +41,75 @@ int client_handshake() {
   return sd;
 }
 
+void boardsetup(int board[8][8], int side) {
+    memset(board, 0, sizeof(int) * 64);
+    board[0][0] = BLACKROOK;
+    board[0][1] = BLACKKNIGHT;
+    board[0][2] = BLACKBISHOP;
+    board[0][3] = BLACKQUEEN;
+    board[0][4] = BLACKKING;
+    board[0][5] = BLACKBISHOP;
+    board[0][6] = BLACKKNIGHT;
+    board[0][7] = BLACKROOK;
+    board[1][0] = BLACKPAWN;
+    board[1][1] = BLACKPAWN;
+    board[1][2] = BLACKPAWN;
+    board[1][3] = BLACKPAWN;
+    board[1][4] = BLACKPAWN;
+    board[1][5] = BLACKPAWN;
+    board[1][6] = BLACKPAWN;
+    board[1][7] = BLACKPAWN;
+    board[7][0] = WHITEROOK;
+    board[7][1] = WHITEKNIGHT;
+    board[7][2] = WHITEBISHOP;
+    board[7][3] = WHITEQUEEN;
+    board[7][4] = WHITEKING;
+    board[7][5] = WHITEBISHOP;
+    board[7][6] = WHITEKNIGHT;
+    board[7][7] = WHITEROOK;
+    board[6][0] = WHITEPAWN;
+    board[6][1] = WHITEPAWN;
+    board[6][2] = WHITEPAWN;
+    board[6][3] = WHITEPAWN;
+    board[6][4] = WHITEPAWN;
+    board[6][5] = WHITEPAWN;
+    board[6][6] = WHITEPAWN;
+    board[6][7] = WHITEPAWN;
+}
+
 int main() {
     
     const int WIDTH = 640;
     const int HEIGHT = 480;
+    struct user cuser;
+    memset(&cuser, 0, sizeof(struct user));
+    int board[8][8];
+    boardsetup(board, 1);
 
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window * window = SDL_CreateWindow("chess", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    SDL_Window * window = SDL_CreateWindow("chess", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
     
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Surface * image = SDL_LoadBMP("pawn.bmp");
-    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
-    int w, h;
+    SDL_Texture * texture;
+    int w, h, r, c;
     int quit = 0;
     
 
     SDL_Event event;
-    SDL_Rect rect[8][8];
+    struct square gboard[8][8];
+    memset(gboard, 0, 64 * sizeof(struct square));
+    SDL_Rect registerbox;
+    registerbox.x = 150;
+    registerbox.y = 0;
+    registerbox.w = 100;
+    registerbox.h = 100;
+    SDL_Rect login;
+    login.x = 0;
+    login.y = 0;
+    login.w = 100;
+    login.h = 100;
+    SDL_StartTextInput();
+    SDL_SetTextInputRect(&login);
     while(!quit) {
         while(SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
@@ -52,51 +118,80 @@ int main() {
             }
             if (event.type == SDL_MOUSEBUTTONDOWN) {
                 if (event.button.button == SDL_BUTTON_LEFT) {
-                    printf("mouse clicked: %d\n", event.button.x);
+                    if (cuser.logged == 1) {
+                        for (r = 0; r < 8; r++) {
+                            for (c = 0; c < 8; c++) {
+                                if (event.button.x > gboard[r][c].rect.x && event.button.x < gboard[r][c].rect.x + gboard[r][c].rect.w && event.button.y > gboard[r][c].rect.y && event.button.y < gboard[r][c].rect.y + gboard[r][c].rect.h) {
+                                    printf("rect[%d][%d] was clicked\n", r, c);
+                                }
+                            }
+                        }
+                    }
+                    //printf("mouse clicked: %d\n", event.button.x);
                 }
             }
             if (event.type == SDL_MOUSEBUTTONUP) {
                 printf("released\n");
+            }
+            if (event.type == SDL_TEXTINPUT) {
+                printf("key: %s\n", event.text.text);
             }
         }
         SDL_GetWindowSize(window, &w, &h);
         //printf("w: %d, h: %d\n", rect.w, rect.h);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
-        int r;
-        int c;
-        for (r = 0; r < 8; r++) {
-            for (c = 0; c < 8; c++) {
-                rect[r][c].x = c*w/8;
-                rect[r][c].y = r*h/8;
-                rect[r][c].w = w/8;
-                rect[r][c].h = h/8;
-                if ((r + c) % 2 == 0) {
-                    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+        if (cuser.logged == 0) {
+            for (r = 0; r < 8; r++) {
+                for (c = 0; c < 8; c++) {
+                    gboard[r][c].rect.x = c*w/8;
+                    gboard[r][c].rect.y = r*h/8;
+                    gboard[r][c].rect.w = w/8;
+                    gboard[r][c].rect.h = h/8;
+                    if ((r + c) % 2 == 0) {
+                        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+                    }
+                    else {
+                        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                    }
+                    SDL_RenderFillRect(renderer, &(gboard[r][c].rect));
+                    switch (board[r][c]) {
+                        case WHITEPAWN:
+                            gboard[r][c].image = SDL_LoadBMP("whitepawn.bmp");
+                            texture = SDL_CreateTextureFromSurface(renderer, gboard[r][c].image);
+                            SDL_RenderCopy(renderer, texture, NULL, &(gboard[r][c].rect));
+                            break;
+                    }
                 }
-                else {
-                    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-                }
-                SDL_RenderFillRect(renderer, &(rect[r][c]));
-                SDL_RenderCopy(renderer, texture, NULL, &(rect[r][c]));
             }
+        }
+        else {
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+//            SDL_RenderFillRect(renderer, &login);
+//            SDL_RenderFillRect(renderer, &registerbox);
         }
         /*
         for (r = 0; r < 8; r++) {
             for (c = 0; c < 8; c++) {
-                printf("rect: [%d][%d], x: %d, y: %d\n", r, c, rect[r][c].x, rect[r][c].y);
+                printf("rect: [%d][%d], x: %d, y: %d\n", r, c, gboard[r][c].rect.x, gboard[r][c].rect.y);
             }
         }
-        sleep(10);
-        */
+        sleep(10);*/
+        
         SDL_RenderPresent(renderer);
     }
-    SDL_FreeSurface(image);
+    for (r = 0; r < 8; r++) {
+        for (c = 0; c < 8; c++) {
+            if (gboard[r][c].image != NULL) {
+                SDL_FreeSurface(gboard[r][c].image);
+            }
+        }
+    }
+    SDL_StopTextInput();
     SDL_Quit();
     return 0;
 
     int sd = client_handshake();
-    struct user cuser;
     char ucommand[500];
     int loginc = 0;
     printf("login or register? ");
